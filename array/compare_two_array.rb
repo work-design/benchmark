ea9c6f90ffe31dfb1b7ba0dbf4966cc8a87391af
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-13 21:45:23 +0800

    project

---
+++ array/compare_two_array.rb
@@ -0,0 +1,19 @@
+require 'benchmark/ips'
+require 'set'
+
+ARRAY = [*1..10]
+OTHER = ARRAY.reverse
+
+def fast
+  (ARRAY - OTHER).empty? && (OTHER - ARRAY).empty?
+end
+
+def slow
+  ARRAY.to_set == OTHER.to_set
+end
+
+Benchmark.ips do |x|
+  x.report('Array相减') { fast }
+  x.report('Array#to_set') { slow }
+  x.compare!
+end
