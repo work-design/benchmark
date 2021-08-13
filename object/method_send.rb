d3b976f245c9ae272a0d103a83e58da2135555f4
qinmingyuan <mingyuan0715@foxmail.com>
2021-08-13 21:50:54 +0800

    compare send

---
+++ object/method_send.rb
@@ -0,0 +1,15 @@
+require 'benchmark/ips'
+
+def fast
+  1.to_s
+end
+
+def slow
+  1.send :to_s
+end
+
+Benchmark.ips do |x|
+  x.report('直接调用') { fast }
+  x.report('send 调用') { slow }
+  x.compare!
+end
