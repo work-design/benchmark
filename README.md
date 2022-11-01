97cca15697f5cf5e6975a8bdf0617b51ba9a1b5a
qinmingyuan <mingyuan0715@foxmail.com>
2022-11-01 15:03:50 +0800

    compact compact vs compact!

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # benchmark
 内部一些技术方案的性能比较
+
+
+## 参考
+
+* [fast-ruby](https://github.com/fastruby/fast-ruby)
---
+++ enum/compact_vs_compact!.rb
@@ -0,0 +1,18 @@
+require 'benchmark/ips'
+
+def slow
+  h = [1, nil]
+  h = h.compact
+end
+
+def fast
+  h = [1, nil]
+  h.compact!
+  h
+end
+
+Benchmark.ips do |x|
+  x.report('Array#compact') { slow }
+  x.report('Array#compact!') { fast }
+  x.compare!
+end
